1b8ad93d4b0389c6a8e5d97b6ab4c9a953068f22
Ahmad Beyranvand <ahmadb1881@gmail.com>
2025-01-28 19:18:33 +0330

    Add Grade Report View page

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->middleware("guest");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/gradeReportView', function () {
+    return view('school.grade_report');
+})->middleware(['auth', 'verified'])->name('grade_report_view');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
