7a231e66f27afaf3eba2861e37644973b291fdf2
Ahmad Beyranvand <ahmadb1881@gmail.com>
2025-01-30 10:30:36 +0330

    Create new migration

--- database/migrations/2025_01_29_190627_create_classrooms_table.php
+++ database/migrations/2025_01_29_190627_create_classrooms_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->tinyInteger('level');
+            $table->tinyInteger('field_id');
             $table->timestamps();
         });
     }
