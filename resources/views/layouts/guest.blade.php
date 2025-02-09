d9574ac7b8f4a72d98d74765230f3921c0a037df
Ahmad Beyranvand <ahmadb1881@gmail.com>
2025-02-09 17:37:02 +0330

    Design Footer of HavirWeb ™

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,16 +24,26 @@
     </style>
 </head>
 
-<body>
+<body class="relative">
     <main class="w-full min-h-screen bg-cover flex lg:flex-row flex-col"
         style="background: url('/static/img/school.webp') no-repeat center center;">
         <div x-transition
             class="flex lg:min-h-screen min-h-[50vh] xl:w-9/12 lg:p-0 py-24 w-full bg-black bg-opacity-[50%] filter backdrop-blur flex flex-col text-center items-center justify-center">
-            <img src="/static/img/logo.svg"
-                class="glow w-[260px]  xl:w-[500px] lg:w-[300px]" alt="">
+            <img src="/static/img/logo.svg" class="glow w-[260px]  xl:w-[500px] lg:w-[300px]" alt="">
+            <div class="md:flex hidden justify-center bg-white absolute bottom-5 w-[98%] left-0 right-0 mx-auto p-3 ">
+                طراحی شده در استودیوی طراحی
+                <a class="text-purple-600" href="https://havirweb.ir">هاویر وب</a>
+                ™
+            </div>
         </div>
-        <div class="xl:w-3/12 lg:min-h-screen min-h-[50vh] w-full p-12 bg-white flex flex-col justify-center items-center">
+        <div
+            class="xl:w-3/12 lg:min-h-screen min-h-[50vh] w-full p-12 bg-white flex flex-col justify-center items-center">
             {{ $slot }}
+        </div>
+        <div class="md:hidden flex justify-center bg-white absolute bottom-5 w-[98%] left-0 right-0 mx-auto p-3 ">
+            طراحی شده در استودیوی طراحی
+            <a class="text-purple-600 mx-1" href="https://havirweb.ir">هاویر وب</a>
+            ™
         </div>
     </main>
     <script type="module" src="/static/js/vfx.js"></script>
